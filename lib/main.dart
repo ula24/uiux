646808e3ba83a3b035c8ecac9a4bd5d9e4c257a3
Ula24 <ulashanshal2@gmail.com>
2022-02-23 10:07:11 +0300

    container inside container

--- lib/main.dart
+++ lib/main.dart
@@ -14,26 +14,41 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
           body: SafeArea(
-
             child:Container(
-              width: 200,
-              height: 200,
-                decoration: const BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.only(topLeft:Radius.circular(20),topRight: Radius.circular(0),bottomLeft: Radius.circular(0),bottomRight: Radius.circular(32))),
-            child:Container(
-              width: 150,
-              height: 150,
+              width: 400,
+              height: 400,
+              padding: EdgeInsets.all(40),
               decoration: const BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.only(topLeft:Radius.circular(7),topRight: Radius.circular(7),bottomLeft: Radius.circular(7),bottomRight: Radius.circular(7))),
-            )
+                  color: Colors.red,
+                  borderRadius: BorderRadius.only(topLeft:Radius.circular(20),topRight: Radius.circular(0),bottomLeft: Radius.circular(0),bottomRight: Radius.circular(32))),
+
+              child: Container(
+                width: 150,
+                height: 150,
+                padding: EdgeInsets.all(40),
+                decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(topLeft:Radius.circular(20),topRight: Radius.circular(0),bottomLeft: Radius.circular(0),bottomRight: Radius.circular(32))),
+
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  padding: EdgeInsets.all(40),
+                  decoration: const BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.only(topLeft:Radius.circular(20),topRight: Radius.circular(0),bottomLeft: Radius.circular(0),bottomRight: Radius.circular(32))),
+
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                      decoration: const BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.only(topLeft:Radius.circular(20),topRight: Radius.circular(0),bottomLeft: Radius.circular(0),bottomRight: Radius.circular(32))),
+                    child:Center(child: Text('red')) ,
+                  ),
+                ),
+              ),
             ),
-
-
-
-
-                //child: Center(child: Text('red')),),
             ) ,
           ),
 
